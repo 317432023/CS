aff80669b065601d0c7632a4ad1294bb41b28c2c
kangtengjiao <317432023@qq.com>
2024-03-31 13:47:58 +0800

    h2sql语句支持

--- cs-admin/src/main/java/com/laolang/cs/chatmessage/controller/ChatMessageController.java
+++ cs-admin/src/main/java/com/laolang/cs/chatmessage/controller/ChatMessageController.java
@@ -16,6 +16,7 @@ import com.laolang.cs.server.authen.SubsysTool;
 import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,14 +36,16 @@ public class ChatMessageController {
     private ChatMessageReadService readService;
     private ChatUserService chatUserService;
     private SubsysTool subsysTool;
+    private Environment environment;
 
     /**
      * 取未读消息数目(当前用户)
+     *
      * @return
      */
-    @ApiOperation(value="取未读消息数目(当前用户)")
+    @ApiOperation(value = "取未读消息数目(当前用户)")
     @ApiImplicitParams({
-        @ApiImplicitParam(paramType = "header", name = "clientId", value = "客戶端ID", dataType = "string"),
+            @ApiImplicitParam(paramType = "header", name = "clientId", value = "客戶端ID", dataType = "string"),
     })
     @GetMapping("unread_message_num")
     public R<Long> unreadMessageNum(@RequestHeader String clientId) {
@@ -56,18 +59,21 @@ public class ChatMessageController {
 
     /**
      * 取聊天用户名单(在线情况)
+     *
      * @return
      */
-    @ApiOperation(value="取聊天用户名单(在线情况)")
+    @ApiOperation(value = "取聊天用户名单(在线情况)")
     @ApiImplicitParams({
-        @ApiImplicitParam(paramType = "header", name = "clientId", value = "客戶端ID", dataType = "string"),
+            @ApiImplicitParam(paramType = "header", name = "clientId", value = "客戶端ID", dataType = "string"),
     })
     @GetMapping("chat_users")
     public R<List<Map<String, Object>>> onlineChatUsers(@RequestHeader String clientId) {
         ChatUser senderChatUser = subsysTool.getChatUser(clientId); // 消息發送者
         List<ChatUser> rcptList = chatUserService.queryRcptIdList(senderChatUser.getId());
         Date date = DateUtil.parse("1970", "yyyy");
-        rcptList.forEach(e-> {if(e.getLastMessageTime()==null)e.setLastMessageTime(date);});
+        rcptList.forEach(e -> {
+            if (e.getLastMessageTime() == null) e.setLastMessageTime(date);
+        });
         //rcptList.stream().sorted(Comparator.comparing(ChatUser::getLastMessageTime).reversed().thenComparing(ChatUser::getId));
 
         List<Map<String, Object>> rcptMapList = new ArrayList<>();
@@ -79,20 +85,17 @@ public class ChatMessageController {
             onlineChatUserMap.put("rcptId", rcpt.getId());
             onlineChatUserMap.put("nickName", rcpt.getNickName());
 
-            String imgUrl = rcpt.getAvatar() != null?rcpt.getAvatar().trim():"";
-            if(StringUtils.isNotBlank(imgUrl) && !imgUrl.startsWith("http://") && !imgUrl.startsWith("https://")) {
+            String imgUrl = rcpt.getAvatar() != null ? rcpt.getAvatar().trim() : "";
+            if (StringUtils.isNotBlank(imgUrl) && !imgUrl.startsWith("http://") && !imgUrl.startsWith("https://")) {
                 imgUrl = staticDomain + (imgUrl.startsWith("/") ? "" : "/") + imgUrl;
             }
 
             onlineChatUserMap.put("avatar", imgUrl); // 头像
 
             Boolean online;
-            if(onlineCache.containsKey(rcpt.getId()))
-            {
+            if (onlineCache.containsKey(rcpt.getId())) {
                 online = onlineCache.get(rcpt.getId());
-            }
-            else
-            {
+            } else {
                 online = subsysTool.checkOnline(rcpt.getId());
                 onlineCache.put(rcpt.getId(), online);
             }
@@ -103,7 +106,7 @@ public class ChatMessageController {
             QueryWrapper<ChatMessageRead> qw = new QueryWrapper<>();
             qw.eq("`read`", false);
             qw.eq("rcpt_id", senderChatUser.getId());
-            qw.exists("select 1 from tb_chat_message where id=msg_id and sender="+rcpt.getId());
+            qw.exists("select 1 from tb_chat_message where id=msg_id and sender=" + rcpt.getId());
             long count = readService.count(qw);
             onlineChatUserMap.put("unreadMessageNum", count);
 
@@ -115,22 +118,23 @@ public class ChatMessageController {
 
     /**
      * 取最近聊天历史
-     * @param clientId socket连接令牌
+     *
+     * @param clientId           socket连接令牌
      * @param customerChatUserId 聊天用戶ID 客服可以使用该参数；会员忽略此参数
-     * @param headMsgId 已取得的最早消息ID
-     * @param limit 条数，默认15条
+     * @param headMsgId          已取得的最早消息ID
+     * @param limit              条数，默认15条
      * @return
      */
-    @ApiOperation(value="取最近聊天历史")
-    @ApiResponses( value = {
-      @ApiResponse(code = 0/200, message = "OK"),
-      @ApiResponse(code = 1, message = "FAIL"),
+    @ApiOperation(value = "取最近聊天历史")
+    @ApiResponses(value = {
+            @ApiResponse(code = 0 / 200, message = "OK"),
+            @ApiResponse(code = 1, message = "FAIL"),
     })
     @ApiImplicitParams({
-      @ApiImplicitParam(paramType="header", name = "clientId", value = "客戶端ID", dataType = "string"),
-      @ApiImplicitParam(paramType="query", name = "customerChatUserId", value = "聊天用戶ID(客服可以使用该参数；会员忽略此参数)", dataType = "int"),
-      @ApiImplicitParam(paramType="query", name = "headMsgId", value = "已取得的最早消息ID", dataType = "bigint"),
-      @ApiImplicitParam(paramType="query", name = "limit", value = "最多返回記錄數", dataType = "int"),
+            @ApiImplicitParam(paramType = "header", name = "clientId", value = "客戶端ID", dataType = "string"),
+            @ApiImplicitParam(paramType = "query", name = "customerChatUserId", value = "聊天用戶ID(客服可以使用该参数；会员忽略此参数)", dataType = "int"),
+            @ApiImplicitParam(paramType = "query", name = "headMsgId", value = "已取得的最早消息ID", dataType = "bigint"),
+            @ApiImplicitParam(paramType = "query", name = "limit", value = "最多返回記錄數", dataType = "int"),
     })
     @GetMapping("recent_messages")
     public R<List<ChatMessage>> recentMessages(@RequestHeader String clientId,
@@ -140,34 +144,35 @@ public class ChatMessageController {
         ChatUser chatUser = subsysTool.getChatUser(clientId);
         int userType = chatUser.getUserType();
         List<ChatMessage> chatMessageList;
-        QueryWrapper<ChatMessage> qw = new QueryWrapper<>();
+        String readSQL = "h2".equalsIgnoreCase(environment.getProperty("spring.datasource.platform", "mysql")) ?
+                "(case `read` when is null then false else `read` end)" :
+                "ifnull(`read`,false)"; // 是否已读SQL语句 根据数据不同语句不通 由于 h2 数据库不支持 ifnull 函数改用case...end
         String[] selectColumns = new String[]{
-            "id",
-            "sender",
-            "receiver",
-            "message",
-            "create_time",
-            "(select nick_name from tb_chat_user cu where cu.id=sender) senderNickName",
-            userType==1?"(select nick_name from tb_chat_user cu where cu.id=receiver) receiverNickName":"NULL receiverNickName",
-            /* h2 数据库不支持ifnull函数改用case...end
-            "(case sender when "+chatUser.getId()+" then 1 else (select (case `read` when is null then false else `read` end) from tb_chat_message_read cmr where cmr.msg_id=tb_chat_message.id and cmr.rcpt_id="+chatUser.getId()+") end) as `read`"*/
-            "(case sender when "+chatUser.getId()+" then 1 else (select ifnull(`read`,false) from tb_chat_message_read cmr where cmr.msg_id=tb_chat_message.id and cmr.rcpt_id="+chatUser.getId()+") end) as `read`"
+                "id",
+                "sender",
+                "receiver",
+                "message",
+                "create_time",
+                "(select nick_name from tb_chat_user cu where cu.id=sender) senderNickName",
+                userType == 1 ? "(select nick_name from tb_chat_user cu where cu.id=receiver) receiverNickName" : "NULL receiverNickName",
+                "(case sender when " + chatUser.getId() + " then 1 else (select " + readSQL + " from tb_chat_message_read cmr where cmr.msg_id=tb_chat_message.id and cmr.rcpt_id=" + chatUser.getId() + ") end) as `read`"
         };
+        QueryWrapper<ChatMessage> qw = new QueryWrapper<>();
         qw.select(selectColumns);
-        if(userType == 0) { // 客人
+        if (userType == 0) { // 客人
             qw.eq("receiver", chatUser.getId()); // 群ID = 客人 ID
         } else { // 客服
-            if(customerChatUserId != null && customerChatUserId > 0) {
+            if (customerChatUserId != null && customerChatUserId > 0) {
                 ChatUser customerChatUser = chatUserService.getById(customerChatUserId);
                 Assert.isTrue(customerChatUser.getUserType() == 0, "400-仅能查询客人用户的聊天历史");
                 Assert.isTrue(customerChatUser.getTenantId().equals(chatUser.getTenantId()), "403-无权限查询该用户历史");
                 qw.eq("receiver", customerChatUserId); // 群ID = 客人 ID
             } else {
-                qw.exists("select 1 from tb_chat_user cu where cu.id=receiver and cu.tenant_id='"+chatUser.getTenantId()+"'");
+                qw.exists("select 1 from tb_chat_user cu where cu.id=receiver and cu.tenant_id='" + chatUser.getTenantId() + "'");
             }
         }
 
-        if(headMsgId != null && headMsgId > 0) {
+        if (headMsgId != null && headMsgId > 0) {
             qw.lt("id", headMsgId);
             qw.orderByDesc("id"); // 按 ID 倒序排序 优先返回 ID 较大 即最近的记录
         } else {
@@ -175,15 +180,15 @@ public class ChatMessageController {
             qw.orderByDesc("create_time"); // 按 create_time 倒序排序 优先返回 最近的记录
         }
 
-        qw.last("limit "+(limit > 50?50:(limit<=0?15:limit))); // 分页
+        qw.last("limit " + (limit > 50 ? 50 : (limit <= 0 ? 15 : limit))); // 分页
         chatMessageList = service.list(qw);
         Collections.reverse(chatMessageList); // 反转顺序
 
         // 图片如有必要加资源域名前缀
         for (ChatMessage chatMessage : chatMessageList) {
-            if(chatMessage.getMessage() != null && "image".equals(chatMessage.getMessage().get("type"))) {
-                String imgUrl = (String)chatMessage.getMessage().get("value");
-                if(StringUtils.isNotBlank(imgUrl) && !imgUrl.startsWith("http://") && !imgUrl.startsWith("https://")) {
+            if (chatMessage.getMessage() != null && "image".equals(chatMessage.getMessage().get("type"))) {
+                String imgUrl = (String) chatMessage.getMessage().get("value");
+                if (StringUtils.isNotBlank(imgUrl) && !imgUrl.startsWith("http://") && !imgUrl.startsWith("https://")) {
                     RedisTool redisTool = SpringContextHolder.getBean("redisTool", RedisTool.class);
                     String staticDomain = redisTool.hget("system_config", "STATIC_DOMAIN", ModeDict.APP_GROUP, 1);
                     imgUrl = staticDomain + (imgUrl.startsWith("/") ? "" : "/") + imgUrl;
